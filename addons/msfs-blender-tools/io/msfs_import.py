0c048b9b17de1e74d0308c87e13df88b3664cf91
pepperoni505 <49950581+pepperoni505@users.noreply.github.com>
2022-04-07 14:10:54 -0400

    feat: add check to import hooks to ensure we only run on built files

--- addons/msfs-blender-tools/io/msfs_import.py
+++ addons/msfs-blender-tools/io/msfs_import.py
@@ -19,18 +19,28 @@ import bpy
 from .msfs_primitive import MSFS_Primitive
 from .msfs_texture import MSFS_Texture
 
+def on_built_asset(func):
+    def inner(*args, **kwargs):
+        gltf = args[-1] # The Khronos importer always passes the glTF as the last argument
+        if gltf.data.asset.extensions and "ASOBO_asset_optimized" in gltf.data.asset.extensions:
+            return func(*args, **kwargs)
+        else:
+            pass
+
+    return inner
+
 class Import:
     def __init__(self):
         pass
 
-    # TODO: add check if optimized mesh
-
+    @on_built_asset
     def gather_import_scene_before_hook(self, gltf_scene, blender_scene, gltf):
         # Overwrite certain import settings
         gltf.import_settings['merge_vertices'] = True # Having this set to False gives us some shading issues
         gltf.import_settings['guess_original_bind_pose'] = False # Having this set to True causes lots of skinning issues
         MSFS_Texture.convert_textures(gltf)
 
+    @on_built_asset
     def gather_import_scene_after_animation_hook(self, gltf_scene, blender_scene, import_settings):
         # Undo some things done during the standard Khronos import process
         for obj in bpy.data.objects:
@@ -43,12 +53,15 @@ class Import:
             if obj.type == "MESH":
                 obj.data.use_auto_smooth = False
 
+    @on_built_asset
     def gather_import_decode_primitive(self, gltf_mesh, gltf_primitive, skin_idx, import_settings):
         MSFS_Primitive.decode_primitive(import_settings, gltf_mesh, gltf_primitive)
 
+    @on_built_asset
     def gather_import_mesh_options(self, mesh_options, gltf_mesh, skin_idx, import_settings):
         mesh_options.skin_into_bind_pose = False # The MSFS build process already calculates bind pose on the vertex locations, so if we do it again it will cause many visual errors
 
+    @on_built_asset
     def gather_import_image_after_hook(self, gltf_img, blender_image, import_settings):
         MSFS_Texture.rename_image(import_settings, gltf_img, blender_image)
         MSFS_Texture.convert_normal_map(import_settings, gltf_img, blender_image)
